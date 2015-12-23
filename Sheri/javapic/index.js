96c52fb33c5037a739c1c9c0a7e99551b53c2773
SheriDover <sheridover@gmail.com>
2015-12-23 13:32:51 -0800

    finished index.js.

--- Sheri/javapic/index.js
+++ Sheri/javapic/index.js
@@ -1,46 +1,44 @@
-/* Previously I used junk.js. I didn't like it because of the array. This was harder to make work.
-Scroll through images so that we are using numbers 01-60 and starting over, but passing any number that doesn't exist.
-event listener that calls nextImage function after the window is loaded*/
+/* Previously I used junk.js. I didn't like it because of the array. This was harder to make work,
+because I was trying to use setTimer which didn't work when I when I tried to reset loop.
+Worked in junk.js because I didn't get to where I tried to reset the loop.
+Below is the event listener that calls nextImage function after the window is loaded*/
 window.onload = nextImage;
-/*nextImage function iterates through integers to create links for the div with id jumbotron
- the links are inserted in background-image of the css */
-function runJumbo(i) {
-
-  setTimeout(function() {
-
-    // this changes the css with class jumbotron and changes this image assigned to it.
-    var changeIt = document.getElementById('jumbotron');
-    //here we put a 0 in front of a single digit integer to match the 01 -09 images
-   if (i < 10){
-    changeIt.style.backgroundImage="url(images/pdxcg_" + "0" + i + ".jpg)";
-    console.log("url(images/pdxcg_" + "0" + i + ".jpg)");
-    /*here we skip the missing image. A little simple and not useful if other images disappear.
-    could be fixed later. Evan says changing it isn't in the scope of the assignment;
-    we just need to make sure a missing image doesn't break the code.*/
-  } else if (i === 42){
-    changeIt.style.backgroundImage="url(images/pdxcg_43.jpg)";
-  }
-  //here we just insert double digit integers
-  else if (i > 10){
-     changeIt.style.backgroundImage="url(images/pdxcg_" + i + ".jpg)";
-     console.log("url(images/pdxcg_" + i + ".jpg)");
-  }
-  //this is the last half of the timer. every 1000 = one second.  * i creates a queue so that the first is in 1 second, the second in two seconds...
-}, 1000 * i);
-
+function nextImage() {
+  var picNum = 1;
+  var changeIt = document.getElementById('jumbotron');
+ //setInterval is a built-in timer function in JavaScript. Inside it I place the rules for the images.
+  setInterval(function() {
+    /*conditional (ternary) operator (?:)
+    returns on of two expressions depinging on condition - a shortcut for an if/else
+    below translates to if picNum equals 60, then picNum is reassigned to equal 1,
+    then add 1 to picNum each time the anonymous function runs.
+    same as:
+    if (picNum === 60) {
+    picNum = 1:
+  } else {
+  picNum += 1;
+}
+  */
+    picNum = picNum === 60 ? 1 : picNum + 1;
+    //the first line of this conditional fixes the problem of the leading zeros in the first 9 images.
+    if (picNum < 10){
+      //this makes a new image url for the css background-image
+      changeIt.style.backgroundImage="url(images/pdxcg_" + "0" + picNum + ".jpg)";
+      //this is a de-bugger. I'm leaving it in in case I need to debug later.
+        console.log("url(images/pdxcg_" + "0" + picNum + ".jpg)");
+        /*here we skip the missing image. A little simple and not useful if other images disappear.
+        could be fixed later. Evan says changing it isn't in the scope of the assignment;
+        we just need to make sure a missing image doesn't break the code the code ran fine
+        before I added this, so it should still */
+      } else if (picNum === 42){
+        changeIt.style.backgroundImage="url(images/pdxcg_43.jpg)";
+      }
+      //here we just insert double digit integers
+      else if (picNum > 9){
+         changeIt.style.backgroundImage="url(images/pdxcg_" + picNum + ".jpg)";
+         console.log("url(images/pdxcg_" + picNum + ".jpg)");
+      }
+      //and the last half of setInterval. 10000 = 10 seconds.
+  }, 10000);
 
 }
-
-function nextImage() {
-
-  // a for loop that iterates through integers
-      for (var i = 1; i < 61; i++) {
-        // var x = 1;
-        //setTimeout calls  nextImage function every 4 seconds. This goes with the 4000 * i at the bottom.
-      runJumbo(i);
-      //this has to go in a different function because we're changing i back to 1 to start over.
-      // if (i === 60){
-      //   i = 1;
-      //}
-  }
-};
