1878331ce60210406b99cfd875d435ee9a2463fe
pdxgitit <charles.webster04@gmail.com>
2015-12-29 19:41:20 -0800

    29v2

--- Charles/week8/randomquote/random.js
+++ Charles/week8/randomquote/random.js
@@ -1,7 +1,10 @@
+getaquote();
+
 function getaquote () {
   var test = new XMLHttpRequest ();
   if (XMLHttp.status === 200){
     test.open("get", `../api-3.0.json`, true);
     test.send(null);
+    console.log("test");
   }
 }
