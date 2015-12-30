00b3c93d58f245672cd761f7295dd96bfea1c3cb
pdxgitit <charles.webster04@gmail.com>
2015-12-29 21:12:35 -0800

    23224ee2433

--- Charles/week8/randomquote/random.js
+++ Charles/week8/randomquote/random.js
@@ -1,11 +1,13 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "./api-3.0.json";
+var apiUrl = "http://quotesondesign.com/api/3.0/api-3.0.json";
 
 xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var myArr = JSON.parse(xmlhttp.responseText);
+    if (xmlhttp.status == 200) {
+        var apiDump = JSON.parse(xmlhttp.responseText);
     }
-    console.log(myArr);
+    console.log(apiDump);
 };
-xmlhttp.open("GET", url, true);
+xmlhttp.open("GET", apiUrl, true);
 xmlhttp.send();
+
+document.getElementById("quote").innerHTML = apiDump;
