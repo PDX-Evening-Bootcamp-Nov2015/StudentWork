6d62a731ee9713330bed741d263435a8065ee012
carlieb <ball.carlie@gmail.com>
2015-12-29 20:36:37 -0800

    Update

--- Carolyn/Week8/random_quote/random_quote.js
+++ Carolyn/Week8/random_quote/random_quote.js
@@ -1,14 +1,17 @@
-window.onload = function(){
+window.onload = function() {
+  document.getElementById('newQuote').onclick = randomQuote;
+};
+
+function randomQuote(){
     var xmlhttp = new XMLHttpRequest();
-    var url = "nrs.json";
+    var url = "http://quotesondesign.com/api/3.0/api-3.0.json";
 
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var obj = JSON.parse(xmlhttp.responseText);
             document.getElementById("demo").innerHTML =
-            obj.name + "<br>" +
-            obj.street + "<br>" +
-            obj.phone;
+            obj.quote + "<br>" +
+            obj.author;
         };
     };
     xmlhttp.open("GET", url, true);
