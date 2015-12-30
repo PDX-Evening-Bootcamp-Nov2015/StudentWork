326ccf42b8c058016d79b734a25c57d0ba0269a0
pdxgitit <charles.webster04@gmail.com>
2015-12-29 20:57:49 -0800

    23224e3

--- Charles/week8/randomquote/random.js
+++ Charles/week8/randomquote/random.js
@@ -4,7 +4,6 @@ var url = "./api-3.0.json";
 xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         var myArr = JSON.parse(xmlhttp.responseText);
-        myFunction(myArr);
     }
 };
 xmlhttp.open("GET", url, true);
