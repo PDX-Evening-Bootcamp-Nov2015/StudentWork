91b92d5c4d8ba42e1d05ea975d07674f0252e7cf
Garrett Skjelstad <garrettskj@gmail.com>
2015-12-30 20:39:45 -0800

    Update 20151230-1

--- Garrett/Week7/javapic/js/gallery.js
+++ Garrett/Week7/javapic/js/gallery.js
@@ -1,20 +1,21 @@
 /*
 - _gallery.html_
-  - update the slogan to add a ", user_name", with the user's name
-    - loop through the image folder and display each image in the folder
-      - add the functionality so that if a user clicks on an image, the lightbox appears with that image loaded in
-        - when the lightbox is up, is the user clicks anywhere not on the image, the lightbox closes
+- update the slogan to add a ", user_name", with the user's name
+- loop through the image folder and display each image in the folder
+- add the functionality so that if a user clicks on an image, the lightbox appears with that image loaded in
+- when the lightbox is up, is the user clicks anywhere not on the image, the lightbox closes
 */
 
 window.onload = function(e){
-    var galleryHTML = document.getElementById("gallery");
-        galleryHTML.addEventListener("click", lightbox);
+    var imageshowHTML = document.getElementById("image_show");
+        imageshowHTML.addEventListener("click", closeLightbox);
     var user = "Garrett";
     var name = document.getElementsByClassName("tagline")[0].innerHTML = "develop something, " + user;
     createImage();
 };
 
 function createImage () {
+    //loop through the number of files to create images with event listeners.
     var fileNum;
     for (var i=1; i<60; i += 1){
         if (i <= 9 ){
@@ -28,16 +29,38 @@ function createImage () {
         var liContainer = document.createElement("li");
         var newImage = document.createElement("img");
         newImage.setAttribute('src', fileName);
-        //newImage.setAttribute('class', 'display_img');
+        newImage.setAttribute('class', 'gallery');
         newImage.addEventListener("click", lightbox);
         galleryHTML.appendChild(liContainer);
         liContainer.appendChild(newImage);
     }
 };
 
+//open the lightbox
 function lightbox () {
+    //get the lightbox prepped
     var showImg = event.target;
-    showImg.setAttribute('class', 'display_img');
-    console.log(showImg);
+    var showImgSrc = showImg.getAttribute('src');
+    var imgBig = document.getElementById("image_show");
+    
+    //set the class on the lightbox for all to see
+    imgBig.firstChild.setAttribute('src', showImgSrc);
+    imgBig.setAttribute('class', 'display_img'); 
+
+    //I don't know why this doesn't work.
+    //imgBig.firstChild = showImg;
+
 };
 
+//close the lightbox
+function closeLightbox () {
+    //get the clicked element
+    var showImg = event.target;
+   
+    // if you click on the DIV, (not but not the image)
+    // otherwise change the class to hide it..
+    if (showImg.getAttribute("id") === "image_show"){
+        var imgBig = document.getElementById("image_show");
+        imgBig.setAttribute('class', 'display_none'); 
+    };
+};
