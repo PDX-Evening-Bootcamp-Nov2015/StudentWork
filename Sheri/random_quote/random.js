a57bbfc86cb81d665d6066666819162fa3a68223
SheriDover <sheridover@gmail.com>
2016-01-04 20:59:02 -0800

    conflicting

--- Sheri/random_quote/random.js
+++ Sheri/random_quote/random.js
@@ -1,27 +1,29 @@
-//window.onload event listener
 
-function apiQuote(){
-  //open a new request for a quote
+//function that requests the api
+function apiQuote() {
+  //new request for a quote. key word new. I forgot that at first. Again.
   var request = new XMLHttpRequest();
+  //requests from the api address.
   request.open("GET", "../../api-3.0.json");
-  //parse the response test into an object
-  request.addEventListener("load",function(){
+  //load the response into the destinations
+  request.addEventListener("load", function() {
     var data = JSON.parse(this.responseText);
-    console.log(data);
-    //grab the quoote text and author
+    //console.log(data);
+    //get the values using the keys
     var quote = data.quote;
-    var author =data.author;
-    //add it to the DOM
-    document.getElementById('quote_div').innerText=quote;
-    document.getElementById('author_div').innerText=author;
+    var author = data.author;
+    //insert into the assigned divs
+    document.getElementById('quote_div').innerText = quote;
+    document.getElementById('author_div').innerText = author;
 
   })
+ //call the function
   request.send();
- }
-
- window.onload = function() {
-   //listener for new button
-     document.getElementById("new").addEventListener("click", function() {
-     apiQuote();
-   });
- };
+}
+//the listener 
+window.onload = function() {
+  //listener for new button
+  document.getElementById("new").addEventListener("click", function() {
+    apiQuote();
+  });
+};
