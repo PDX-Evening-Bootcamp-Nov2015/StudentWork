6bdd48a378640f236db508402738556cbd827db1
pdxgitit <charles.webster04@gmail.com>
2016-01-05 20:39:02 -0800

    vv16

--- Charles/week8/randomquote/random.js
+++ Charles/week8/randomquote/random.js
@@ -2,20 +2,16 @@ window.onload = function (){
   pullQuote();
 }
 
-
-
 function pullQuote (){
 var xmlhttp = new XMLHttpRequest ();
-var url = "http://54.68.253.162/StudentWork/api-3.0.json";
 
 xmlhttp.onreadystatechange = function () {
   if (xmlhttp.status === 200) {
     var quote = JSON.parse(xmlhttp.responseText);
-    console.log(xmlhttp.responseText);
   }
 }
-xmlhttp.open("GET", url, true);
+xmlhttp.open("GET", "http://54.68.253.162/StudentWork/api-3.0.json", true);
 xmlhttp.send();
-console.log("fetched!");
-
+return(quote);
 }
+console.log(quote);
