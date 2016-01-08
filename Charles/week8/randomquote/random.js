42c858f2da5da59f9aac0e57f955d8ae94acea6e
pdxgitit <charles.webster04@gmail.com>
2016-01-07 18:41:05 -0800

    9

--- Charles/week8/randomquote/random.js
+++ Charles/week8/randomquote/random.js
@@ -7,9 +7,9 @@ function pullQuote (event){
 var xmlhttp = new XMLHttpRequest ();
 xmlhttp.onreadystatechange = function () {
   if (xmlhttp.status === 200) {
-    quoteJSON = JSON.parse(xmlhttp.responseText);
-    document.getElementById('quote1').innerHTML = quoteJSON.quote;
-    document.getElementById('author').innerHTML = quoteJSON.author;
+    quoteS = JSON.parse(xmlhttp.responseText);
+    document.getElementById('quote1').innerHTML = quoteS.quote;
+    document.getElementById('author').innerHTML = quoteS.author;
   }
 }
 xmlhttp.open("GET", "http://54.68.253.162/StudentWork/api-3.0.json", true);
