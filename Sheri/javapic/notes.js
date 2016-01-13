c73bb8b04eb763427720c599ac9ca21c0e6987f7
SheriDover <sheridover@gmail.com>
2016-01-12 18:39:18 -0800

    cleaning up javapic file

--- Sheri/javapic/notes.js
+++
@@ -1,8 +0,0 @@
-```javascript
- var request = new XMLHttpRequest();
- request.open("GET", "http://www.foo.com");
- request.addEventListener("load", function () {
-   console.log(this.responseText);
- })
- request.send(); // sometimes you'll see request.send(null) or request.send(someVar=someValue)
-```
