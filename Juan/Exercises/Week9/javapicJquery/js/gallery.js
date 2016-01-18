2aee28b3d719f344b37dd5538a29c5c6dd59b5a7
Juan Espinet <juan.espinet@gmail.com>
2016-01-18 13:17:16 -0800

    username retreival reworked

--- Juan/Exercises/Week9/javapicJquery/js/gallery.js
+++ Juan/Exercises/Week9/javapicJquery/js/gallery.js
@@ -9,60 +9,61 @@ _gallery.html_
   lightbox closes
   */
 
-// function to generate image path
+/* function to generate image path */
 function imgPath(imgNum){
+  imgNum = String(imgNum);
   // check if image number needs zero padding
-  if (imgNum < 10) {
+  if (imgNum.length < 2) {
     imgNum = '0' + imgNum;
-  } else {
-    imgNum = String(imgNum);
   }
   // add properly padded number to path and return
   return 'images/pdxcg_' + imgNum + '.jpg';
 }
 
-// function to generate new li containing image
+/* function to generate new li containing image */
 function genImg(imgNum){
   // add alt image
 }
 
-// function to populate gallery
+/* function to populate gallery */
 function popGallery(numImgs) {
 
 }
 
-// function to parse username from url
+/* function to parse username from url */
 function getUserName() {
   // check the url and store it in a variable so we can search it
-  var pageURL = window.location.href,
-      uNameRegEx = /[?&]+([^?&]+)/g, // regex to match strings after ? in url
-      passedData = pageURL.match(uNameRegEx); // cache all the matching strings
+  var url = $(location)attr.('href'),
+      urlComponents,
+      username;
+  // split the url into searchable components
+  urlComponents = url.split('?')
   // search the match data for the passed username and return it
-  for (var i = 0; i < passedData.length; i++) {
-    var currStr = passedData[i];
-    // match function returns array, loop through that array to find username
-    if (currStr.substr(0, 9) === '?username') {
-      // break the loop when we find the username
-      return currStr.substr(10)
-    }
+  for (var i = 0; i < urlComponents.length; i++){
+      // split the string again to compare value pairs
+      compComponents = urlComponents[i].split('=');
+      if (compComponents[0] === 'username') {
+        return compComponents[1];
+      }
   }
+
 }
 
-// function to insert the username into the page
+/* function to insert the username into the page */
 function popUName(uName) {
 
 }
 
-// function to select clicked image and display in lightbox
+/* function to select clicked image and display in lightbox */
 function imgClick(event) {
 
 }
 
-// function to close lightbox on a click elsewhere
+/* function to close lightbox on a click elsewhere */
 function closeLightbox(event) {
 
 }
 
 window.onload = function(){
-  
+
 };
